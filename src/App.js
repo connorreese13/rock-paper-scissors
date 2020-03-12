b49c608f802da4c2f35c46dc6017c54d0af6cabe
Connor Reese <connor.reese13@gmail.com>
2020-03-12 15:17:09 +0800

    added html & css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/styles.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <div id="opponent">
+          <div id="rock" class="option "></div>
+          <div id="paper" class="option hidden"></div>
+          <div id="scissors" class="option hidden"></div>
+        </div>
+        <div id="outcome"></div>
+        <div id="player">
+          <button id="rock" class="option" onclick="play('rock')"></button>
+          <button id="paper" class="option" onclick="play('paper')"></button>
+          <button
+            id="scissors"
+            class="option"
+            onclick="play('scissors')"
+          ></button>
+        </div>
+      </body>
     </div>
   );
 }
