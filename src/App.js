4efcdcbdc5c76e240b15203a26d67b04270bb2fa
Connor Reese <connor.reese13@gmail.com>
2020-03-12 16:48:21 +0800

    added logic for rock choice

--- src/App.js
+++ src/App.js
@@ -2,28 +2,63 @@ import React from "react";
 import "./styles/styles.css";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <body>
-        <div id="opponent">
-          <div id="rock" class="option "></div>
-          <div id="paper" class="option hidden"></div>
-          <div id="scissors" class="option hidden"></div>
-        </div>
-        <div id="outcome"></div>
-        <div id="player">
-          <button id="rock" class="option" onclick="play('rock')"></button>
-          <button id="paper" class="option" onclick="play('paper')"></button>
-          <button
-            id="scissors"
-            class="option"
-            onclick="play('scissors')"
-          ></button>
-        </div>
-      </body>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    outcome: "",
+    playerChoice: "",
+    oppenentChoice: "",
+    playerMove: ["rock", "paper", "scissors"],
+    opponentMove: ["rock", "paper", "scissors"]
+  };
+
+  play = () => {
+    //e.preventDefault();
+    console.log(this.state.outcome);
+    const randomMove = this.state.opponentMove;
+    const computerChoice =
+      randomMove[Math.floor(Math.random() * randomMove.length)];
+    const humanMove = this.state.playerMove[0];
+    if (computerChoice === "scissors") {
+      this.setState({ outcome: "You win!" });
+      console.log(this.state.outcome);
+    } else if (computerChoice === "paper") {
+      this.setState({ outcome: "You lose!" });
+    } else {
+      this.setState({ outcome: "it's a draw" });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <body>
+          <div id="opponent">
+            <div id="rock" className="option hidden"></div>
+            <div id="paper" className="option hidden"></div>
+            <div id="scissors" className="option hidden"></div>
+          </div>
+          <div id="outcome">{this.state.outcome}</div>
+          <div id="player">
+            <button
+              id={this.state.playerMove[0]}
+              className="option"
+              onClick={x => this.play()}
+            ></button>
+            <button
+              id="paper"
+              className="option"
+              onClick="play('paper')"
+            ></button>
+            <button
+              id="scissors"
+              className="option"
+              onClick="play('scissors')"
+            ></button>
+          </div>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
